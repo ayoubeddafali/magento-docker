c6404914a567857671b2714725105572a0f73e11
ayoubensalem <ayoubensalem@gmail.com>
2018-10-25 20:37:02 +0100

    fix ssl shell script

--- swarm/certbot.sh
+++
@@ -1,8 +0,0 @@
-docker run --rm \
-  -p 443:443 -p 80:80 --name letsencrypt \
-  -v "/etc/letsencrypt:/etc/letsencrypt" \
-  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-  certbot/certbot certonly -n \
-  -m "ayoubensalem@gmail.com" \
-  -d ayoubensalem.com  \
-  --standalone --agree-tos
